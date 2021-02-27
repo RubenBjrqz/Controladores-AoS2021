a15f5ccd226b061567b8a2a1a00315356edd7860
Rubén Bojórquez <rubenbjrz@gmail.com>
2021-02-26 18:36:04 -0700

    Clase 26/02/2021

--- resources/views/admin/noticias/index.blade.php
+++ resources/views/admin/noticias/index.blade.php
@@ -20,7 +20,30 @@
                     <td>
                         <a href="{{ route("admin.noticias.show", $noticia->id) }}">Ver</a>
                         <a href="{{ route("admin.noticias.edit", $noticia->id) }}">Editar</a>
-                        <a href="{{ route("admin.noticias.confirmdelete", $noticia->id) }}">Eliminar</a>
+                        <button class="btn btn-xs btn-danger" type="button" data-toggle="modal" data-target="#confirmarEliminacion{{$noticia->id}}">Eliminar</button>
+                        <div class="modal fade" id="confirmarEliminacion{{$noticia->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="exampleModalLabel">Confirmación de eliminación de noticia: {{$noticia->id}}</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+                                  <p>¿Esta seguro de eliminar la noticia: "{{$noticia->titulo}}"?</p>
+                                </div>
+                                <div class="modal-footer">
+                                  <form method="POST" action="{{route("admin.noticias.destroy", $noticia->id)}}">
+                                    @csrf 
+                                    @method('delete') 
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                     </td>
                 </tr>
             @endforeach
